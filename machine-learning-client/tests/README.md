49ba698ab7cdbf277a0a552790b56c0f732fcf0b
JonasonWu <jw5911@nyu.edu>
2022-11-22 19:07:31 -0500

    Added test functions as well as how to get coverage

---
+++ machine-learning-client/tests/README.md
@@ -0,0 +1,14 @@
+# Prerequisite
+
+* Make sure that you have completed the [README.md](../README.md) steps for setting up the virtual environment for the ml-client.
+
+# Get Code Coverage information
+
+* cd to the machine-learning-client directory. Assuming you are currently at this directory, run the following:
+    ```
+    cd ../
+    ```
+* Run pytest with coverage
+    ```
+    pytest --cov
+    ```
--- machine-learning-client/tests/test_hand_gesture_rock_paper_scissor.py
+++ machine-learning-client/tests/test_hand_gesture_rock_paper_scissor.py
@@ -1,6 +1,7 @@
 import pytest
 import hand_gesture_rock_paper_scissor as mlgame
 import cv2
+import numpy as np
 
 class TestGameFunctions:
 
@@ -8,9 +9,39 @@ class TestGameFunctions:
         assert True == True, "Sanity check failed"
     def test_sanity_call(self):
         assert mlgame.StaticVariables.BLACK == (0,0,0), "Expected correct call to variable"
-    def test_display_img(self):
-        frame = cv2.flip(cv2.imread('mock_img/rock.jpg'), 1)
-        mlgame.show_frame(frame)
+
+    def test_predict_gesture_rock(self):
+        frame = cv2.imread('./tests/mock_img/rock.jpg')
+        assert type(frame) == np.ndarray, "Expected frame to be successfully fetched"
+        gesture, frame = mlgame.predict_gesture(frame)
+        assert gesture == 'rock', "Expected gesture predicted to be rock"
+
+    def test_predict_gesture_scissor(self):
+        frame = cv2.imread('./tests/mock_img/scissor.jpg')
+        assert type(frame) == np.ndarray, "Expected frame to be successfully fetched"
+        gesture, frame = mlgame.predict_gesture(frame)
+        assert gesture == 'scissor', "Expected gesture predicted to be scissor"
+    
+    def test_predict_gesture_paper(self):
+        frame = cv2.imread('./tests/mock_img/paper.jpg')
+        assert type(frame) == np.ndarray, "Expected frame to be successfully fetched"
+        gesture, frame = mlgame.predict_gesture(frame)
+        assert gesture == 'paper', "Expected gesture predicted to be paper"
+
+    def test_computer_plays(self):
+        props = ['rock', 'paper', 'scissor']
+        for i in range(10):
+            assert mlgame.computer_plays() in props, "Expected computer to play one of the props (rock, paper, scissor)."
+
+    def test_establish_web_cam_connection(self):
+        try:
+            cap = mlgame.establish_web_cam_connection()
+            assert cap != None, "Expected web cam to be fetched"
+        except mlgame.WebCamConnection as e:
+            assert True
+        assert False, "Expected exception to be thrown since web cam cannot be successfully fetched."
+        
+
 
 class TestFrameFunctions:
     pass
