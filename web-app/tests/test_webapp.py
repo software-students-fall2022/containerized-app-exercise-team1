bbccdda7a2f447d1b470d12737b3fefb248a3560
aleolazabal <ajolazabal01@gmail.com>
2022-11-27 00:08:31 -0400

    removed incorrect code

--- web-app/tests/test_webapp.py
+++ web-app/tests/test_webapp.py
@@ -15,18 +15,6 @@ game = [
     }
 ]
 
-cxn = MongoClient("mongodb://127.0.0.1:27017")
-try:
-    # verify the connection works by pinging the database
-    cxn.admin.command('ping') # The ping command is cheap and does not require auth.
-    db = cxn['ml_client'] # store a reference to the database
-    print(' *', 'Connected to MongoDB!') # if we get here, the connection worked!
-
-except Exception as e:
-    # the ping command failed, so the connection is not available.
-    # render_template('error.html', error=e) # render the edit template
-    print('Database connection error:', e) # debug
-
 
 def test_base_route():
 
